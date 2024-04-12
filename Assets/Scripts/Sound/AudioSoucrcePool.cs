748eec60da9664970da29c5cd9674deba5ea425e
GitKaspar <127048948+GitKaspar@users.noreply.github.com>
2024-04-12 20:19:32 +0300

    Basic Sound scripts

---
+++ Assets/Scripts/Sound/AudioSoucrcePool.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioSourcePool : MonoBehaviour
+{
+    public static AudioSourcePool Instance;
+
+    public AudioSource AudioSourcePrefab;
+
+    private List<AudioSource> audioSources;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        audioSources = new List<AudioSource>();
+        Instance = this;
+    }
+
+    public AudioSource GetSource()
+    {
+        foreach (AudioSource source in audioSources)
+        {
+            if (!source.isPlaying)
+            {
+                return source;
+            }
+        }
+        AudioSource newSource = GameObject.Instantiate(AudioSourcePrefab, transform);
+        audioSources.Add(newSource);
+
+        return newSource;
+    }
+}
